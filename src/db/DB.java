4a2a222127f6ea714eb79b4b293ac2b3b9dfd097
Guilherme Luan <guilhermeluan@pm.me>
2023-09-19 19:16:23 -0300

    feat: getConnection

--- src/db/DB.java
+++ src/db/DB.java
@@ -3,9 +3,26 @@ package db;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DB {
+
+    private static Connection connection = null;
+    public static Connection getConnection(){
+        if(connection == null){
+            try {
+                Properties properties = loadProperties();
+                String url = properties.getProperty("dburl");
+                connection = DriverManager.getConnection(url, properties);
+            } catch (SQLException e){
+                throw new DbExecption(e.getMessage());
+            }
+        }
+        return connection;
+    }
 
     public static Properties loadProperties(){
         Properties properties = new Properties();
