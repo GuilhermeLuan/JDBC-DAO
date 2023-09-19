1ed1f258deff9fa5aaa3708bdfc020b729b14a0a
Guilherme Luan <guilhermeluan@pm.me>
2023-09-19 19:11:07 -0300

    feat: create loadProperties

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,4 +1,19 @@
 package db;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class DB {
+
+    public static Properties loadProperties(){
+        Properties properties = new Properties();
+        try (FileInputStream fileInputStream = new FileInputStream("db.properties")){
+            properties.load(fileInputStream);
+            return properties;
+        } catch (IOException e){
+            throw new DbExecption(e.getMessage());
+        }
+    }
 }
